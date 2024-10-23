0b05752de9f2b98d297610ec7168e251f3806e74
Aniigutierres <131274361+Aniigutierres@users.noreply.github.com>
2024-10-23 16:33:02 -0300

    pasta 2

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+
+import { View, Text, StyleSheet, Image, TextInput, TouchableOpacity, Alert } from "react-native";
+
+
+export default function Index() {
+   
+    return (
+    
+            <Text>Faça login em sua conta</Text>
+
+    )}
